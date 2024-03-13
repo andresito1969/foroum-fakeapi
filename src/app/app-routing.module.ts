de1b0d4b258c42575f6d8ec1eaa4f90927180add
Andrés <ernestoandresrosillo@gmail.com>
2024-03-13 19:18:39 +0100

    [router] added login/register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { PostDetailComponent } from './posts/post-detail/post-detail.component';
 import { Error404Component } from './error404/error404.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [{
   path: '',
@@ -10,6 +12,12 @@ const appRoutes: Routes = [{
 }, {
   path: 'posts/:id',
   component: PostDetailComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'register',
+  component: RegisterComponent
 }, {
   path: 'error404',
   component: Error404Component
