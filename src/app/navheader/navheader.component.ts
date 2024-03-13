d328f3179d0f6067ca739a5b455d5f17eb0d1391
Andrés <ernestoandresrosillo@gmail.com>
2024-03-13 17:01:55 +0100

    [COMPONENT NAV] ADDED NAV NOT WORKING ATM

---
+++ src/app/navheader/navheader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navheader',
+  templateUrl: './navheader.component.html',
+  styleUrl: './navheader.component.css'
+})
+export class NavheaderComponent {
+
+}
