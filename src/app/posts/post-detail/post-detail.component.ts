a5205b34daed3c0ce0aa05c68a270a16565c69d1
Andrés <ernestoandresrosillo@gmail.com>
2024-03-13 19:10:09 +0100

    [HTTP] single post, template ref ng if else for loading

--- src/app/posts/post-detail/post-detail.component.ts
+++ src/app/posts/post-detail/post-detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit{
+  
+  constructor(private route: ActivatedRoute, 
+    private http: HttpClient){}
 
+  detailedPost;
+  isLoadedPost = false;
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.params.id
+    this.fetchDetailedPost(postId);
+  }
+
+  fetchDetailedPost = (postId) => {
+    let params = new HttpParams().set('id', postId);
+    setTimeout(() => {
+      this.http.get("https://jsonplaceholder.typicode.com/posts", { params: params })
+        .subscribe(post => {
+          this.detailedPost = post[0]
+          this.isLoadedPost = true;
+        });
+    }, 2000)
+    
+  }
 }
